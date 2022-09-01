65349597fd197c719a9d85741646d7ac02ede15a
RaceWilliams3 <willr108@my.erau.edu>
2022-09-01 15:42:33 -0700

    Everything But Final Vid/Build

--- RW_M01_DV01_Cubethon/Assets/Scripts/PlayerMovement.cs
+++ RW_M01_DV01_Cubethon/Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,8 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody rb;
 
-    public float forwardForce = 2000f;
-    public float sidewaysForce = 500f;
+    public float forwardForce = 500f;
+    public float sidewaysForce = 100f;
 
     // Update is called once per frame
     void FixedUpdate()
@@ -15,13 +15,18 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKey("d"))
         {
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 
         }
         if (Input.GetKey("a"))
         {
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
 
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
         }
     }
 }
